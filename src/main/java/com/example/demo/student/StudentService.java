830a5f78bf17d0aa95d737d6eb9583bf9cc970a4
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-12-01 19:03:48 +0600

    Implemented dependency injection, Fixed return statement in StudentController.java, Added self-explanatory comments.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,7 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -8,6 +10,9 @@ import java.util.List;
 // In simple words; this will help to transit data between API layer to data access from database layer.
 // So basically, it's the service layer.
 
+// Telling that this class has to service as a spring bean.
+// @Component works the same but Service is better because of semantics.
+@Service
 public class StudentService {
 
     public List<Student> getStudents() {
