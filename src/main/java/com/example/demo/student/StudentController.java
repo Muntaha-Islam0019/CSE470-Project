3b5935076c7ea896df915406d700f5df077b5105
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-12-30 00:29:54 +0600

    Added update functionality.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,10 +1,7 @@
 package com.example.demo.student;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 // This class is going to have all the resources for the API
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -20,16 +17,25 @@ public class StudentController {
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
-
     @PostMapping // Used in registering new student to the database.
     public void registerNewStudent(@RequestBody Student student) { // Take the request and map it into student.
         studentService.addNewStudent(student);
     }
 
-    // Delete by ID
+    // Delete by ID.
     // The ID will be provided as a path of the url, for instance, DELETE http ... student/1
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
+    }
+
+    //Update students.
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email
+    ) {
+        studentService.updateStudent(studentId, name, email);
     }
 }
