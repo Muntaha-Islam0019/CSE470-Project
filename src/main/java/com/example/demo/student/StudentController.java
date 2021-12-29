9a3633c5be03d2f083d060c6e75d34a13ae08c03
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-12-30 00:18:06 +0600

    Implemented post-mapping.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,19 +1,15 @@
 package com.example.demo.student;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 // This class is going to have all the resources for the API
-
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
-
     private final StudentService studentService;
-
     // Using dependency injection to connect the StudentService to studentService automatically using spring.
     // So the StudentService class has to be a spring bean (to be instantiated).
     @Autowired
@@ -23,5 +19,10 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping // Used in registering new student to the database.
+    public void registerNewStudent(@RequestBody Student student) { // Take the request and map it into student.
+        studentService.addNewStudent(student);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,17 +1,13 @@
 package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 // Mainly responsible for the business logic.
 // In simple words; this will help to transit data between API layer to data access from database layer.
 // So basically, it's the service layer.
-
 // Telling that this class has to service as a spring bean.
 @Service // @Component works the same but Service is better because of semantics.
 public class StudentService {
-
     private final StudentRepository studentRepository;
     @Autowired
     public StudentService(StudentRepository studentRepository) {
@@ -19,5 +15,10 @@ public class StudentService {
     }
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        // For test.
+        System.out.println(student);
     }
 }
