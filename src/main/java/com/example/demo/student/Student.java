305ddd058ca0516981bf593c9e7f42be73d9fa57
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-12-29 19:08:04 +0600

    Added transient and ability to calculate age by itself.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,50 +2,49 @@ package com.example.demo.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity // For hibernate.
 @Table // For table in database.
 public class Student {
-
     @Id
     @SequenceGenerator(
             name = "student_sequence",
             sequenceName = "student_sequence",
             allocationSize = 1
     )
-
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
             generator = "student_sequence"
     )
-
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
-    private Integer age;
+    @Transient
+    private Integer age; // No need to be a column in database
+
     public Student() {
+
     }
     public Student(Long id,
                    String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
+
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
+
     public Long getId() {
         return id;
     }
@@ -70,9 +69,11 @@ public class Student {
     public void setDob(LocalDate dob) {
         this.dob = dob;
     }
+
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
+
     public void setAge(Integer age) {
         this.age = age;
     }
--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,32 +1,26 @@
 package com.example.demo.student;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
-
 // Made for adding students to the database.
 @Configuration
 public class StudentConfig {
-
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
             Student muntaha = new Student(
                     "Muntaha",
                     "muntaha.islam0019@gmail.com",
-                    LocalDate.of(1999, Month.JULY, 19),
-                    22
+                    LocalDate.of(1999, Month.JULY, 19)
             );
 
             Student sameha = new Student(
                     "Sameha",
                     "sameha.kamrul@gmail.com",
-                    LocalDate.of(2000, Month.DECEMBER, 2),
-                    22
+                    LocalDate.of(2000, Month.DECEMBER, 2)
             );
 
             repository.saveAll(
