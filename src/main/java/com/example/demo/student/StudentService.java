aeb4e78eeb7e32064a79411d0fb7e0838d4c68bf
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-12-29 18:55:41 +0600

    implemented JPA repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,29 +1,22 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
-
-// Mainly responsible for the business logic.
-// In simple words; this will help to transit data between API layer to data access from database layer.
-// So basically, it's the service layer.
-
-// Telling that this class has to service as a spring bean.
-// @Component works the same but Service is better because of semantics.
-@Service
+@Service // @Component works the same but Service is better because of semantics
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Muntaha",
-                        "muntaha.islam0019@gmail.com",
-                        LocalDate.of(1999, Month.JULY, 19),
-                        22
-                )
-        );
+        return studentRepository.findAll();
     }
 }
